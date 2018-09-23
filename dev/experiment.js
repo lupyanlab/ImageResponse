8a0818a23e7eff7d300b4fa1667448ae0c37fc7b
Kevin Mui <kmui2@wisc.edu>
2018-09-22 23:31:37 -0500

    updated instructions

--- dev/experiment.js
+++ dev/experiment.js
@@ -79,9 +79,7 @@ function runExperiment(trials, workerId, assignmentId, hitId, PORT, FULLSCREEN) 
     key_forward: 'space',
     key_backward: 'backspace',
     pages: [
-      `<p>On each page you will see two pictures from the same category e.g., two cats. Your task is simply to decide which of the two pictures is the best example of your idea or image of what the category is. The categories you will see are <b>cats, dogs, birds, fish, cars, trains, planes and boats</b>.
-      <p><b>Use the keys 1-5 to respond</b>. You will be asked to about 225 judgments. Please don't spend too much time on any one set of images. Estimated total time is 10-13 minutes. At the end, you will get a completion code. <br><br><b>Please respond carefully. If you miss the attention checks, we may deny payment.</b>
-            </p> ${continue_space}`
+      `<p>On each page you will see an image with a response and you decide whether the image matches the response.</p> ${continue_space}`
     ]
   };
 
